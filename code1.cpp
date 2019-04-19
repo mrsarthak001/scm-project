28a2952bf66b3e80a164b766d369d0a3c37dd9ef
sarthak <mrsarthak001@gmail.com>
2019-04-19 14:49:48 +0530

    Class contain function for food

--- code1.cpp
+++ code1.cpp
@@ -8,3 +8,31 @@
 #include<iomanip .h>
 #include<stdlib .h>
 
+
+
+//********************************************
+// THIS CLASS CONTAINS FUNTIONS FOR FOOD
+//********************************************
+
+
+class food
+{
+private:
+    fstream p1;
+    int c;
+    char ap;
+    struct fd
+    {
+        char name[55];
+        float price;
+    } f;
+public:
+    food()
+    {
+        c=0;
+    }
+    void food_menu(void);
+    void app_fmenu(void);
+    void food_bill();
+    void del_all();
+};
