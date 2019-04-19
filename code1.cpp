7b7eb424c20d62ecb5a7a6ce1b2ccbb434a53295
sarthak <mrsarthak001@gmail.com>
2019-04-19 14:44:31 +0530

    preprocessor directives

---
+++ code1.cpp
@@ -0,0 +1,10 @@
+#include<iostream .h>
+#include<conio .h>
+#include<string .h>
+#include<graphics .h>
+#include<dos .h>
+#include<stdio .h>
+#include<fstream .h>
+#include<iomanip .h>
+#include<stdlib .h>
+
