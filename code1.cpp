200f3508d88f9c87472cbfed21e758b05225ef68
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2019-04-22 14:09:50 +0530

    INFORMATION  RELATED TO CUSTOMER

--- code1.cpp
+++ code1.cpp
@@ -36,3 +36,37 @@ public:
     void food_bill();
     void del_all();
 };
+//*************
+// THIS CLASS CONTAINS INFORMATION
+//   RELATED TO CUSTOMER
+//*************
+
+
+class customer
+{
+private:
+    int q,w;
+    fstream f1;
+    struct cust
+    {
+        int c_no;
+        char c_name[20];
+        char c_add[80];
+        int a_date;
+        int a_month;
+        int a_year;
+        int d_date;
+        int d_month;
+        int d_year;
+        int room_no;
+        char room_type[25];
+    } p;
+public:
+    food j;
+
+    customer()
+    {
+        p.c_no=0;
+        p.d_date=0;
+        p.d_month=0;
+    }
