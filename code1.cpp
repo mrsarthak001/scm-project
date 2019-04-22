e075ed58f640c6f398627b188703622c1279b9e6
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2019-04-22 20:05:27 +0530

    FUNCTION FOR FOOD BILL

--- code1.cpp
+++ code1.cpp
@@ -72,6 +72,76 @@ public:
         p.d_month=0;
     }
 
+    //*****************************
+//  FUNCTION FOR FOOD BILL
+//*****************************
+
+void food::food_bill()
+{
+    double bill=-1;
+    char c_name[20],f_name[20];
+    int dt;
+    cleardevice();
+    setfillstyle(7,1);
+    floodfill(0,0,4);
+    setfillstyle(7,10);
+    bar(17,40,605,420);
+    rectangle(17,40,605,420);
+    setfillstyle(1,7);
+    bar(24,47,598,413);
+    rectangle(24,47,598,413);
+    setcolor(4);
+    settextstyle(7,0,1);
+    outtextxy(30,70,"ENTER CUSTOMER NAME ");
+    gotoxy(50,6);
+    cin>>c_name;
+    outtextxy(30,120,"ENTER ITEM NAME TAKEN");
+    gotoxy(50,9);
+    cin>>f_name;
+    outtextxy(30,170,"ENTER THE QUANTITY");
+    gotoxy(50,12);
+    cin>>dt;
+    p1.close();
+    p1.open("food.txt",ios::in|ios::binary);
+    while(p1.read((char*)&f,sizeof(f)))
+    {
+        if(strcmp(f.name,f_name)==0)
+        {
+            bill=dt*f.price;
+        }
+    }//END OF WHILE
+    if(bill==-1)
+    {
+        setcolor(1);
+
+        for(int i=0; i<=20; i++)
+        {
+            setcolor(1);
+            outtextxy(30,220,"ITEM IS NOT PRESENT");
+            delay(100);
+            setcolor(WHITE);
+            outtextxy(30,220,"ITEM IS NOT PRESENT");
+            delay(100);
+            //  delay(2500);
+        }
+    }
+    else
+    {
+        char t[5],b[5];
+        setcolor(1);
+        itoa(dt,t,10);
+        itoa(bill,b,10);
+        outtextxy(30,250,"NAME FOOD.NAME      QUANTITY      BILL ");
+        setcolor(1);
+        settextstyle(4,0,1);
+        outtextxy(30,280,c_name);
+        outtextxy(140,280,f_name);
+        outtextxy(320,280,t);
+        outtextxy(445,280,b);
+        getch();
+    }
+}
+    
 //*************
 //  FUNCTION TO APPEND IN FOOD MENU
 //*************
